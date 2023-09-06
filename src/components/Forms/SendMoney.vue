bcd79f828d2ad3320b5849fecbe03cecfc48a5ce
nzivo <johnnnzivo@gmail.com>
2023-09-06 11:38:40 +0300

    Added formkit

--- src/components/Forms/SendMoney.vue
+++ src/components/Forms/SendMoney.vue
@@ -1,25 +1,5 @@
-<template>
-  <form-wizard @on-complete="onComplete" color="#000000">
-    <tab-content title="Personal details"> My first tab content </tab-content>
-    <tab-content title="Additional Info"> My second tab content </tab-content>
-    <tab-content title="Last step">
-      Yuhuuu! This seems pretty damn simple
-    </tab-content>
-  </form-wizard>
-</template>
+<template></template>
 
 <script>
-import { FormWizard, TabContent } from "vue3-form-wizard";
-import "vue3-form-wizard/dist/style.css";
-export default {
-  components: {
-    FormWizard,
-    TabContent,
-  },
-  methods: {
-    onComplete() {
-      alert("Yay. Done!");
-    },
-  },
-};
+export default {};
 </script>
