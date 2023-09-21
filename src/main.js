723cdd06ed9922692013b6159e710592f472f576
nzivo <johnnnzivo@gmail.com>
2023-09-22 00:57:18 +0300

    Added an error reporting service

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import router from "./router";
+import store from "./store/index";
 // Imported tailwind css file
 import "./assets/tailwind.css";
+import * as Sentry from "@sentry/vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+// Added an error reporting service
+Sentry.init({
+  app,
+  dsn: "https://6198749d79af7e83e5fc0b828a4a4c10@o4505920675971072.ingest.sentry.io/4505920677347328",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+app.use(router, store);
+
+app.mount("#app");
