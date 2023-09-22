642102e9b63ab4ff8572393c4219b4070426063e
nzivo <johnnnzivo@gmail.com>
2023-09-22 04:35:31 +0300

    Added auth pages

--- src/views/auth/ForgottenPassword.vue
+++ src/views/auth/ForgottenPassword.vue
@@ -1,4 +1,77 @@
-<template></template>
+<template>
+  <!-- Container -->
+  <div class="container mx-auto">
+    <div class="flex justify-center px-6 my-12">
+      <!-- Row -->
+      <div class="w-full xl:w-3/4 lg:w-11/12 flex">
+        <!-- Col -->
+        <div
+          class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
+          style="
+            background-image: url('https://source.unsplash.com/oWTW-jNGl9I/600x800');
+          "
+        ></div>
+        <!-- Col -->
+        <div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
+          <div class="px-8 mb-4">
+            <h3 class="pt-4 mb-2 text-2xl">Forgot Your Password?</h3>
+            <p class="mb-4 text-sm text-gray-700">
+              We get it, stuff happens. Just enter your email address below and
+              we'll send you a link to reset your password!
+            </p>
+          </div>
+          <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+            <div class="mb-4">
+              <label
+                class="block mb-2 text-sm font-bold text-gray-700"
+                for="email"
+              >
+                Email
+              </label>
+              <input
+                class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Enter Email Address..."
+              />
+            </div>
+            <div class="mb-6 text-center">
+              <button
+                class="w-full px-4 py-2 font-bold text-white bg-green-700 rounded hover:bg-green-900 focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Reset Password
+              </button>
+            </div>
+            <hr class="mb-6 border-t" />
+            <div class="text-center">
+              <h6 class="text-sm text-gray-500">
+                Don't have an account?
+                <RouterLink
+                  :to="{ name: 'registration' }"
+                  class="inline-block text-sm text-green-700 align-baseline hover:text-green-800 hover:font-bold underline-with-svg"
+                >
+                  Create an Account!
+                </RouterLink>
+              </h6>
+            </div>
+            <div class="text-center">
+              <h6 class="text-sm text-gray-500">
+                Already have an account?
+                <RouterLink
+                  :to="{ name: 'login' }"
+                  class="inline-block text-sm text-green-700 align-baseline hover:text-green-800 hover:font-bold underline-with-svg"
+                >
+                  Login!
+                </RouterLink>
+              </h6>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,5 +1,92 @@
 <template>
-  <h1>Login</h1>
+  <!-- <div class="container w-full mx-auto"> -->
+  <div class="flex justify-center">
+    <!-- Row -->
+    <div class="w-full flex">
+      <!-- Col -->
+      <div
+        class="w-full h-auto bg-gray-400 hidden lg:block lg:w-2/3 bg-cover"
+        style="
+          background-image: url('https://source.unsplash.com/K4mSJ7kc0As/600x800');
+        "
+      ></div>
+      <!-- Col -->
+      <div class="w-full lg:w-1/3 bg-white p-5">
+        <h3 class="px-8 pt-6 text-2xl font-bold">Welcome Back!</h3>
+        <h6 class="px-8 text-sm text-gray-500">
+          The most secure, easiest and fastest way to transfer money
+        </h6>
+        <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+          <div class="mb-4">
+            <label
+              class="block mb-2 text-sm font-bold text-gray-700"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div class="mb-4">
+            <label
+              class="block mb-2 text-sm font-bold text-gray-700"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+            <p class="text-xs italic text-red-500">Please choose a password.</p>
+          </div>
+          <div class="mb-4">
+            <input
+              class="mr-2 leading-tight"
+              type="checkbox"
+              id="checkbox_id"
+            />
+            <label class="text-sm" for="checkbox_id"> Remember Me </label>
+          </div>
+          <div class="mb-6 text-center">
+            <button
+              class="w-full px-4 py-2 font-bold text-white bg-green-700 rounded hover:bg-green-900 focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+          </div>
+          <hr class="mb-2 border-t" />
+          <div class="text-end">
+            <RouterLink
+              :to="{ name: 'forgotPassword' }"
+              class="inline-block text-sm text-green-700 align-baseline hover:text-green-800 hover:font-bold"
+            >
+              Forgot Password?
+            </RouterLink>
+          </div>
+        </form>
+        <div class="px-8">
+          <h6 class="text-sm text-gray-500">
+            Don't have an account?
+            <RouterLink
+              :to="{ name: 'registration' }"
+              class="inline-block text-sm text-green-700 align-baseline hover:text-green-800 hover:font-bold underline-with-svg"
+            >
+              Create an Account!
+            </RouterLink>
+          </h6>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- </div> -->
 </template>
 
 <script>
